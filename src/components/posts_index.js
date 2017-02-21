31598519e9ad40eac57ae4ac7e6644d82d8eca22
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-20 22:22:50 -0800

    implements call with lifecycle method

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
   componentWillMount() {
-    console.log("good time to call an action creator to fetch posts");
+    this.props.fetchPosts();
   }
   render() {
     return (
@@ -11,4 +13,5 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+
+export default connect(null, { fetchPosts })(PostsIndex);
