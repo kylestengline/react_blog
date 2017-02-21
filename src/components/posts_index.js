c017078c52e605b69d60b8260a593b25492fdf25
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-20 22:00:40 -0800

    adds lifecycle method

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>List of Blog posts</div>;
-};
+class PostsIndex extends Component {
+  componentWillMount() {
+    console.log("good time to call an action creator to fetch posts");
+  }
+  render() {
+    return (
+      <div>Lists of blog posts</div>
+    );
+  }
+}
+
+export default PostsIndex;
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,9 +5,6 @@ const INITIAL_STATE = {all: [], post: null  };
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_POSTS:
-      //new object,
-      //take current state and add in this property:
-      //all posts as action.payload.data
       return { ...state, all: action.payload.data }
       
     default:
