bdaef3876436e7399d73a005008d9b78910c6c0b
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-20 19:17:45 -0800

    configures react router objects

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { Router, browserHistory } from 'react-router';
+//browserHistory tells react router how to interpret changes to the url.
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistroy} />
   </Provider>
   , document.querySelector('.container'));
