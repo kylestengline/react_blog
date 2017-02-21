24e7a98ce4b3c1abcd03fd9c83cda9af8897c23a
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-20 22:52:24 -0800

    adds redux form, imports form to index reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+//import reduxform, grab reducer property off it, then create var called formReducer.
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
