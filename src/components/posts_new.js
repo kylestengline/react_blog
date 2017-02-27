bbfd698f1fffa1953bc4fa2ec7ccd34e862a47f2
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-26 20:12:31 -0800

    configures form for taking input

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
-import { redxuForm } from 'redux-form';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    //es6 syntax
+    //configuration object for form.
+    const { fields: { title, categories, content },  handleSubmit } = this.props;
+
     return (
-        <form>
+        /*handleSubmit handles form input and 
+        validates all inputs are present.
+        this comes from reduxForm*/
+        <form onSubmit={handleSubmit}>
           <h3>Create a New Post</h3>
+          
           <div className="form-group">
             <label>Title</label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" {...title} />
+            /*we're passing title into the input to make sure that all 
+              properties, on the title object, show up inside the input.*/
           </div>
 
           <div className="form-group">
             <label>Categories</label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" {...categories} />
           </div>
 
           <div className="form-group">
             <label>Content</label>
-            <textarea className="form-control" />
+            <textarea className="form-control"{...content} />
           </div>
 
           <button type="submit" className="btn btn-primary">Submit</button>
