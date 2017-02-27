b00cee530045df9b7211fa3d4d9f836a1c463a13
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-26 20:38:27 -0800

    adds functionality for submitting input in form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CEATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=asdfhjkl';
@@ -12,4 +13,16 @@ export function fetchPosts(){
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+//Take some props from the form and post them to this endpoint.
+/*When we post to the url, we need send along the props as well
+that is why we pass in the second argument "props".*/
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
@@ -7,18 +8,22 @@ class PostsNew extends Component {
     //configuration object for form.
     const { fields: { title, categories, content },  handleSubmit } = this.props;
 
+    //using reduxForm to manage each our 3 inputs.
     return (
         /*handleSubmit handles form input and 
         validates all inputs are present.
         this comes from reduxForm*/
-        <form onSubmit={handleSubmit}>
+        /*we're passing title into the input to make sure that all 
+          properties, on the title object, show up inside the input.*/
+        /*handlSubmit gets called when we submit the form and 
+         handles the props if the form is valid
+         and also calls the action creator.*/
+        <form onSubmit={handleSubmit(this.props.createPost)}>
           <h3>Create a New Post</h3>
           
           <div className="form-group">
             <label>Title</label>
             <input type="text" className="form-control" {...title} />
-            /*we're passing title into the input to make sure that all 
-              properties, on the title object, show up inside the input.*/
           </div>
 
           <div className="form-group">
@@ -37,10 +42,16 @@ class PostsNew extends Component {
   }
 }
 
+/*by defining different fields, we got 3 properites injected 
+into our props object. fields.title fields.categories, etc.*/
 //handling the state side of the app.
+
+//connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+//reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is
+//mapDispatchToProps
 export default reduxForm({ 
   form: 'PostsNewForm', 
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
 //whenever the user is making changes to the input, the changes are
 //affecting the state of the app as well.
