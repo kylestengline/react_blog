59a4a205f241d29a37867093a47299e309219f96
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-26 19:54:43 -0800

    creates form for new posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { redxuForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>Create Form</div>    
+        <form>
+          <h3>Create a New Post</h3>
+          <div className="form-group">
+            <label>Title</label>
+            <input type="text" className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <label>Categories</label>
+            <input type="text" className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <label>Content</label>
+            <textarea className="form-control" />
+          </div>
+
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     );
   }
 }
 
-export default PostsNew;
+//handling the state side of the app.
+export default reduxForm({ 
+  form: 'PostsNewForm', 
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
+//whenever the user is making changes to the input, the changes are
+//affecting the state of the app as well.
