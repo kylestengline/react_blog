4ab622e03051c6eca103be1571c88cc9039479b4
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-27 20:31:09 -0800

    implements delete function with redirect on successful delete

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -9,7 +9,8 @@ class PostsNew extends Component {
   static contextTypes = {
     router: PropTypes.object
   };
-onSubmit(props) {
+
+  onSubmit(props) {
     this.props.createPost(props)
       .then(() => { 
         /*blog post has been created, navigate the user to index.
